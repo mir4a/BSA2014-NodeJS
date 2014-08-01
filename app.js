90d866b8490e079a5201b8a3172b25280876e0be
Miroslav Martynov <marty.mircha@gmail.com>
2014-08-01 10:48:03 +0300

    :running: :dash: :poop:

--- app.js
+++ app.js
@@ -1,98 +1,144 @@
-var express = require('express'),
-  app = express(),
-  http = require('http'),
+var http = require('http'),
   fs = require('fs'),
   url = require('url'),
   querystring = require('querystring'),
   util = require('util'),
-  bodyParser = require('body-parser'),
-  hotelService = require('./hotelService'),
-  connect = require('connect'),
-  methodOverride = require('method-override');
-
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded());
-app.use(methodOverride(function (req, res) {
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    var method = req.body._method;
-    delete req.body._method;
-    return method
-  }
-}));
+  hotelService = require('./hotelService');
 
 
-/**
- * Get particular country
- */
-app.get('/restapi/country/:name', function (req, res) {
-  var data = hotelService.getCountryData(req.params.name, req.query);
-  if (data) {
-    res.send(data);
-  } else {
-    res.send('No country were found with name: ' + req.params.name);
-  }
-});
 
 
-/**
- * Delete particular country
- */
-app.delete('/restapi/country', function (req, res) {
-  console.log(req.body);
-  hotelService.deleteCountry(req.body.country.id);
-  res.redirect('/restapi/country');
-  res.end();
-});
+//
+//
+///**
+// * Delete particular hotel in :name country
+// */
+//app.delete('/restapi/country/:name', function (req, res) {
+//  console.log(req.body);
+//  hotelService.deleteHotel(req.body);
+//  res.redirect('/restapi/country');
+//  res.end();
+//});
+//
+///**
+// * Add new hotel in :name country
+// */
+//app.post('/restapi/country/:name', function (req, res) {
+//  console.log(req.params);
+//  console.log(req.body);
+//  hotelService.addHotel({country: req.params.name, hotel: req.body.hotel.name});
+//  res.redirect('/restapi/country');
+//  res.end();
+//});
 
-/**
- * Delete particular hotel in :name country
- */
-app.delete('/restapi/country/:name', function (req, res) {
-  console.log(req.body);
-  hotelService.deleteHotel(req.body);
-  res.redirect('/restapi/country');
-  res.end();
-});
-
-/**
- * Add new hotel in :name country
- */
-app.post('/restapi/country/:name', function (req, res) {
-  console.log(req.params);
-  console.log(req.body);
-  hotelService.addHotel({country: req.params.name, hotel: req.body.hotel.name});
-  res.redirect('/restapi/country');
-  res.end();
-});
-
-/**
- * Update hotel in particular country
- */
-app.put('/restapi/country', function (req, res) {
-  console.log(req.body);
-  hotelService.updateHotel(req.body);
-  res.redirect('/restapi/country');
-  res.end();
-});
-
-/**
- * Help page
- */
-app.get('/help', function (req, res) {
-  var text = [
-    '<hr>', '<b>localhost:3000/restapi/country</b>', 'returns the list of available countries', '<br />', '<b>localhost:3000/restapi/country/%name%</b>', 'where %name% is name of the country from the list', '<br />', '<b>localhost:3000/restapi/country/%name%/%id%</b>', 'where <b>%id%</b> is a id of the hotel in <b>%name%</b> country', '<br />', '<hr>'];
-  res.send(text.join('<br />'));
-
-});
-
-app.listen(3000);
-console.log('Server started on port 3000. Go to localhost:3000 for further instructions');
 
 var server = http.createServer().listen(8888, 'localhost');
 
 server.on('request', function(req,res) {
-  var url_parts = url.parse(req.url, true);
+  var url_parts = url.parse(req.url, true),
+    path_parts = url_parts.pathname.split('/');
+  console.log('//////////////////path_parts');
+  console.log(path_parts);
+
+  if (path_parts[1] === 'restapi') {
+      console.log("path_parts[1] === 'restapi'");
+    if (path_parts[2] === 'country') {
+      console.log("path_parts[2] === 'country'");
+      if (path_parts[3] !== undefined) {
+        console.log("path_parts[3] !== undefined");
+        if (req.method == 'POST') {
+          var fullBody = '';
+
+          req.on('data', function(chunk) {
+            fullBody += chunk.toString();
+          });
+          req.on('end', function() {
+            var decodedBody = querystring.parse(fullBody);
+            console.log(decodedBody);
+          });
+        } else if (req.method == 'GET') {
+          /**
+           * Get particular country
+           */
+          var fullBody = '';
+
+          req.on('data', function(chunk) {
+            fullBody += chunk.toString();
+          });
+          req.on('end', function() {
+            var decodedBody = querystring.parse(fullBody);
+            console.log('======== decodedBody ========');
+            console.log(decodedBody);
+
+            var data = hotelService.getCountryData(path_parts[3], {hotel: decodedBody['hotel']});
+
+            if (data) {
+              res.send(data);
+            } else {
+              res.send('No country were found with name: ' + req.params.name);
+            }
+          });
+        }
+
+      } else {
+        if (req.method == 'POST') {
+
+          var fullBody = '';
+
+          req.on('data', function(chunk) {
+            fullBody += chunk.toString();
+          });
+          req.on('end', function() {
+
+            var decodedBody = querystring.parse(fullBody);
+            var params;
+            switch(decodedBody['_method']) {
+              case 'put':
+                params = {
+                  country: {
+                    name: decodedBody['country[name]']
+                  },
+                  hotel: {
+                    id: decodedBody['hotel[id]']
+                  }
+                };
+                hotelService.updateHotel(params);
+                break;
+              case 'delete':
+                hotelService.deleteCountry(decodedBody['country[id]']);
+                break;
+              default:
+                /**
+                 * Add new country
+                 */
+                hotelService.addCountry(decodedBody['country[name]']);
+                console.log('By default _method undefined, so node please call addCountry');
+            }
+
+            console.log(decodedBody);
+            res.statusCode = 302;
+            res.setHeader('Location', '/index.html');
+
+            res.end();
+          });
+
+        } else if (req.method == 'GET') {
+
+          /**
+           * Get all countries
+           */
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.end(JSON.stringify(hotelService.getCountryData()));
+        } else {
+          console.log("[501] " + req.method + " to " + req.url);
+          res.writeHead(501, "Not implemented", {'Content-Type': 'text/html'});
+          res.end('<html><head><title>501 - Not implemented</title></head><body><h1>Not implemented!</h1></body></html>');
+        }
+      }
+
+    }
+
+  }
 
   switch(url_parts.pathname) {
     case '/' :
@@ -107,47 +153,6 @@ server.on('request', function(req,res) {
     case '/styles/style.css' :
       res.writeHead(200, {'Content-Type': 'text/css'});
       res.end(fs.readFileSync('public/styles/style.css'));
-      break;
-    case '/restapi' :
-      console.log("[501] " + req.method + " to " + req.url);
-      res.writeHead(501, "Not implemented", {'Content-Type': 'text/html'});
-      res.end('<html><head><title>501 - Not implemented</title></head><body><h1>Not implemented!</h1></body></html>');
-      break;
-    case '/restapi/country' :
-      if (req.method == 'POST') {
-
-        /**
-         * Add new country
-         */
-
-        var fullBody = '';
-
-        req.on('data', function(chunk) {
-          fullBody += chunk.toString();
-        });
-        req.on('end', function() {
-
-          var decodedBody = querystring.parse(fullBody);
-          console.log(decodedBody);
-          res.statusCode = 302;
-          res.setHeader('Location', '/index.html');
-          hotelService.addCountry(decodedBody['country[name]']);
-
-          res.end();
-        });
-
-      } else if (req.method == 'GET') {
-
-        /**
-         * Get all countries
-         */
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(JSON.stringify(hotelService.getCountryData()));
-      } else {
-        console.log("[501] " + req.method + " to " + req.url);
-        res.writeHead(501, "Not implemented", {'Content-Type': 'text/html'});
-        res.end('<html><head><title>501 - Not implemented</title></head><body><h1>Not implemented!</h1></body></html>');
-      }
       break;
     default:
       res.writeHead(404, "Not found", {'Content-Type': 'text/html'});
